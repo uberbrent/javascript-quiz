f6fb15b6d9068b2c441ac3893eb74cd9f6d16aa1
uberbrent <bmjohns1337@gmail.com>
2020-09-24 12:48:05 -0500

    set initial javascript to display beginning screen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,4 +4,31 @@
 // Build a timer
 // localStorage to save highscores
 
-var 
+var questionBoxEl = document.querySelector('#js-flexbox');
+var answersBoxEl = document.querySelector('#js-flexbox');
+var timerBoxEl = document.querySelector('#js-flexbox');
+var pageContentEl = document.querySelector('#page-content')
+
+var startPhase = function() {
+    // create element
+    var h2ItemEl = document.createElement("h2");
+    h2ItemEl.className = "questions"
+    h2ItemEl.textContent = "Welcome to the best quiz ever!"
+
+    var startTextEl = document.createElement("p");
+    startTextEl.className = "instructions"
+    startTextEl.textContent = "After clicking the start button, we will begin a timer of 120 seconds. Answer the questions as quickly as possible! If you answer incorrectly you will be docked time. Your score will be determined by the time remaining. Good luck!"
+    
+    var startButton = document.createElement("button");
+    startButton.textContent = "Begin!"
+    startButton.className = "start-button"
+
+    // display element
+    questionBoxEl.appendChild(h2ItemEl);
+    questionBoxEl.appendChild(startTextEl);
+    questionBoxEl.appendChild(startButton);
+};
+
+pageContentEl.addEventListener('click', startButton);
+
+startPhase();
